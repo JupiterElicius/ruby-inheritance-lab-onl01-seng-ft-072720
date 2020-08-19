7f545d9a3daec94cb9f7729a528dfb45fffbb33c
Nick Bazaldua <bazalnick@gmail.com>
2020-08-19 10:53:33 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,13 @@
 class User
 
-attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name
 
-def self.first_name
-  @first_name = first_name
-end
+  def self.first_name
+    @first_name = first_name
+  end
 
-def self.last_name
-  @last_name = last_name
-end
-
-
-
+  def self.last_name
+    @last_name = last_name
+  end
 
 end
